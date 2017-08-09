f62776820e9380b63fe21d0f682f6da0824432f4
Jacob <jacob@chartiq.com>
2017-08-09 13:45:28 -0400

    370 Create Java sample project
    Add quotefeed and all operations

--- src/com/chartiq/sample/ChartIQ.java
+++ src/com/chartiq/sample/ChartIQ.java
@@ -1,8 +1,12 @@
 package com.chartiq.sample;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.chartiq.sample.model.OHLCChart;
+import com.google.gson.Gson;
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
 import com.teamdev.jxbrowser.chromium.JSAccessible;
@@ -16,6 +20,10 @@ public class ChartIQ {
 
 	Browser browser;
 	BrowserView browserView;
+	private DataSource dataSource;
+	private ArrayList<OnPullInitialDataCallback> onPullInitialData = new ArrayList<>();
+	private ArrayList<OnPullUpdateCallback> onPullUpdate = new ArrayList<>();
+	private ArrayList<OnPullPaginationCallback> onPullPagination = new ArrayList<>();
 
 	JavaObject javaObject = new JavaObject();
 
@@ -34,8 +42,8 @@ public class ChartIQ {
 				window.asObject().setProperty("QuoteFeed", ChartIQ.this);
 				// browser.executeJavaScript("testing()");
 
-				browser.executeJavaScript("attachQuoteFeed(0)");
-				//browser.executeJavaScript("nativeQuoteFeed(parameters, cb)");
+				browser.executeJavaScript("attachQuoteFeed(1)");
+				// browser.executeJavaScript("nativeQuoteFeed(parameters, cb)");
 				// executeJavascript("nativeQuoteFeed(parameters, cb)", null);
 			}
 
@@ -68,22 +76,115 @@ public class ChartIQ {
 	public void testJS() {
 		System.out.println("TESTING JAVASCRIPT BRIDGE");
 	}
-	
+
 	public interface DataSourceCallback {
-        void execute(OHLCChart[] data);
-    }
-	
+		void execute(OHLCChart[] data);
+	}
+
+	public ArrayList<OnPullInitialDataCallback> getOnPullInitialDataCallbacks() {
+		return onPullInitialData;
+	}
+
+	public ArrayList<OnPullUpdateCallback> getOnPullUpdateCallbacks() {
+		return onPullUpdate;
+	}
+
+	public ArrayList<OnPullPaginationCallback> getOnPullPaginationCallbacks() {
+		return onPullPagination;
+	}
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	public interface DataSource {
-        void pullInitialData(Map<String, Object> params, DataSourceCallback callback);
-        void pullUpdateData(Map<String, Object> params, DataSourceCallback callback);
-        void pullPaginationData(Map<String, Object> params, DataSourceCallback callback);
-    }
+		void pullInitialData(Map<String, Object> params, DataSourceCallback callback);
+
+		void pullUpdateData(Map<String, Object> params, DataSourceCallback callback);
+
+		void pullPaginationData(Map<String, Object> params, DataSourceCallback callback);
+	}
 
 	@JSAccessible
 	public void pullInitialData(final String symbol, int period, String interval, String start, String end, Object meta,
 			final String id) {
 		System.out.println(symbol);
 		System.out.println(period);
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("symbol", symbol == null ? "" : symbol);
+		params.put("period", period);
+		params.put("interval", interval);
+		params.put("start", start == null ? "" : start);
+		params.put("end", end == null ? "" : end);
+		params.put("meta", meta);
+		
+		if (dataSource != null) {
+			dataSource.pullInitialData(params, new DataSourceCallback() {
+				@Override
+				public void execute(OHLCChart[] data) {
+					ChartIQ.this.invokePullCallback(id, data);
+				}
+			});
+		}
+	}
+
+	@JSAccessible
+	public void pullUpdate(final String symbol, int period, String interval, String start, Object meta,
+			final String callbackId) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("symbol", symbol == null ? "" : symbol);
+		params.put("period", period);
+		params.put("interval", interval);
+		params.put("start", start == null ? "" : start);
+		params.put("meta", meta);
+
+		if (dataSource != null) {
+			dataSource.pullUpdateData(params, new DataSourceCallback() {
+				@Override
+				public void execute(OHLCChart[] data) {
+					ChartIQ.this.invokePullCallback(callbackId, data);
+				}
+			});
+		}
+	}
+
+	@JSAccessible
+	public void pullPagination(final String symbol, int period, String interval, String start, String end, Object meta,
+			final String callbackId) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("symbol", symbol == null ? "" : symbol);
+		params.put("period", period);
+		params.put("interval", interval);
+		params.put("start", start == null ? "" : start);
+		params.put("end", end == null ? "" : end);
+		params.put("meta", meta);
+
+		if (dataSource != null) {
+			dataSource.pullPaginationData(params, new DataSourceCallback() {
+				@Override
+				public void execute(OHLCChart[] data) {
+					ChartIQ.this.invokePullCallback(callbackId, data);
+				}
+			});
+		}
+	}
+
+	private void invokePullCallback(String callbackId, OHLCChart[] data) {
+		String json = new Gson().toJson(data);
+		browser.executeJavaScript("parseData('" + json + "', \"" + callbackId + "\");");
+	}
+
+	interface OnPullInitialDataCallback {
+		void execute(String symbol, int period, String timeUnit, Date start, Date end, Object meta);
+	}
+
+	interface OnPullUpdateCallback {
+		void execute(String symbol, int period, String timeUnit, Date start, Object meta);
+	}
+
+	interface OnPullPaginationCallback {
+		void execute(String symbol, int period, String timeUnit, Date start, Date end, Object meta);
 	}
 
 	public class JavaObject {
--- src/com/chartiq/sample/ChartIQSample.java
+++ src/com/chartiq/sample/ChartIQSample.java
@@ -1,5 +1,22 @@
 package com.chartiq.sample;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import com.chartiq.sample.ChartIQ.DataSourceCallback;
+import com.chartiq.sample.model.OHLCChart;
+import com.google.gson.Gson;
 import com.teamdev.jxbrowser.chromium.BrowserCore;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
 import com.teamdev.jxbrowser.chromium.internal.Environment;
@@ -45,8 +62,6 @@ public class ChartIQSample extends Application {
 		hb.getChildren().addAll(label1, textField, button);
 		hb.setSpacing(10);
 		hb.setStyle("-fx-background-color: #336699;");
-
-		
 
 		// Browser browser = new Browser();
 		// BrowserView view = new BrowserView(browser);
@@ -114,6 +129,98 @@ public class ChartIQSample extends Application {
 				// browser.executeJavaScript("window.callNewChart('ibm');");
 			}
 		});
+
+		chartIQ.setDataSource(new ChartIQ.DataSource() {
+			@Override
+			public void pullUpdateData(Map<String, Object> params, DataSourceCallback callback) {
+				loadChartData(params, callback);
+			}
+
+			@Override
+			public void pullPaginationData(Map<String, Object> params, DataSourceCallback callback) {
+				loadChartData(params, callback);
+			}
+
+			@Override
+			public void pullInitialData(Map<String, Object> params, DataSourceCallback callback) {
+				loadChartData(params, callback);
+			}
+		});
+	}
+
+	private void loadChartData(Map<String, Object> params, final ChartIQ.DataSourceCallback callback) {
+		if (!params.containsKey("start") || params.get("start") == null || "".equals(params.get("start"))) {
+			params.put("start", "2016-12-16T16:00:00.000Z");
+		}
+
+		if (params.containsKey("end") || "".equals(params.get("start"))) {
+			TimeZone tz = TimeZone.getTimeZone("UTC");
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+			df.setTimeZone(tz);
+			String endDate = df.format(new Date());
+			params.put("end", endDate);
+		}
+
+		boolean isMinute = params.containsKey("interval");
+		// && TextUtils.isDigitsOnly(String.valueOf(params.get("interval")));
+		params.put("interval", isMinute ? "minute" : params.get("interval"));
+		params.put("period", isMinute ? "minute" : params.get("period"));
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("http://simulator.chartiq.com/datafeed?");
+		builder.append("identifier=" + params.get("symbol"));
+		builder.append("&startdate=" + params.get("start"));
+		if (params.containsKey("end")) {
+			builder.append("&enddate=" + params.get("end"));
+		}
+		builder.append("&interval=" + params.get("interval"));
+		builder.append("&period=" + params.get("period"));
+		builder.append("&seed=1001");
+
+		final String url = builder.toString();
+		final String symbol = String.valueOf(params.get("symbol"));
+
+		ExecutorService executor = Executors.newFixedThreadPool(25);
+
+		Runnable runnableTask = () -> {
+			String body = "";
+			try {
+				System.out.println("URL: " + url);
+				URL connectionUrl = new URL(url);
+				HttpURLConnection connection = (HttpURLConnection) connectionUrl.openConnection();
+				connection.setRequestMethod("GET");
+				connection.setRequestProperty("Content-Type", "application/json");
+				connection.connect();
+				int code = connection.getResponseCode();
+				
+				InputStream is;
+				StringBuilder response;
+				if (code >= 200 && code < 400) {
+					response = new StringBuilder();
+					is = connection.getInputStream();
+				} else {
+					is = connection.getErrorStream();
+					response = new StringBuilder("Error(" + code + "): ");
+				}
+				if (is != null) {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+					String line = "";
+					while ((line = reader.readLine()) != null) {
+						response.append(line);
+					}
+					body = response.toString();
+					
+					OHLCChart[] data = new Gson().fromJson(body, OHLCChart[].class);
+                    callback.execute(data);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				OHLCChart[] data = {};
+                callback.execute(data);
+			}
+		};
+
+		executor.execute(runnableTask);
 	}
 
 	public static void main(String[] args) {
