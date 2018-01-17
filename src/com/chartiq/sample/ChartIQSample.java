e5c2496c34bce472c8cee218ac4649a61e4c98b2
jacob <jacob@jacob-VirtualBox>
2018-01-17 14:37:59 -0500

    Add fxml example

--- src/com/chartiq/sample/ChartIQSample.java
+++ src/com/chartiq/sample/ChartIQSample.java
@@ -1,6 +1,9 @@
 package com.chartiq.sample;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -29,12 +32,16 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -48,104 +55,139 @@ public class ChartIQSample extends Application {
 		if (Environment.isMac()) {
 			BrowserCore.initialize();
 		}
-	}
+	} 
 
 	@Override
-	public void start(final Stage primaryStage) {
-		Label label1 = new Label("Symbol:");
+	public void start(final Stage primaryStage) throws IOException {
+		
+		// Create the FXMLLoader 
+				FXMLLoader loader = new FXMLLoader();
+				// Path to the FXML File
+				String fxmlDocPath = "fxml_example.fxml";
+				
+				//root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+				FileInputStream fxmlStream;
+				try {
+					//fxmlStream = new FileInputStream(fxmlDocPath);
+					
+					// Create the Pane and all Details
+					VBox root = (VBox) FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
 
-		primaryStage.setTitle("ChartIQ Example");
-		TextField textField = new TextField();
-		textField.setPrefSize(50, 28);
-		Button button = new Button("Lookup");
-		HBox hb = new HBox();
-		hb.getChildren().addAll(label1, textField, button);
-		hb.setSpacing(10);
-		hb.setStyle("-fx-background-color: #336699;");
+					// Create the Scene
+					Scene scene = new Scene(root);
+					// Set the Scene to the Stage
+					primaryStage.setScene(scene);
+					// Set the Title to the Stage
+					primaryStage.setTitle("A simple FXML Example");
+					// Display the Stage
+					primaryStage.show();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-		// Browser browser = new Browser();
-		// BrowserView view = new BrowserView(browser);
-		//
-		// BorderPane borderPane = new BorderPane();
-		// borderPane.setCenter(view);
-		// borderPane.setTop(hb);
-		//
-		// String remoteDebuggingURL = browser.getRemoteDebuggingURL();
-		//
-		// JFrame frame1 = new JFrame();
-		// frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		// frame1.add(view, BorderLayout.CENTER);
-		// frame1.setSize(700, 500);
-		// frame1.setLocationRelativeTo(null);
-		// frame1.setVisible(true);
-
-		// Scene scene = new Scene(borderPane, 700, 500);
-		// primaryStage.setScene(scene);
-		// primaryStage.show();
-
-		chartIQ.setBrowser("http://192.168.1.147:8080/default/sample-template-native-sdk.html");
-		BrowserView browserView = chartIQ.getBrowserView();
-
-		// Creates another Browser instance and loads the remote Developer
-		// Tools URL to access HTML inspector.
-		// Browser browser2 = new Browser();
-		// BrowserView browserView2 = new BrowserView(browser2);
-
-		// JFrame frame2 = new JFrame();
-		// frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		// frame2.add(browserView2, BorderLayout.CENTER);
-		// frame2.setSize(700, 500);
-		// frame2.setLocationRelativeTo(null);
-		// frame2.setVisible(true);
-
-		// browser2.loadURL(browser.getRemoteDebuggingURL());
-
-		System.out.println(chartIQ.getBrowser().getRemoteDebuggingURL());
-
-		BorderPane pane = new BorderPane();
-		pane.setTop(hb);
-		pane.setCenter(browserView);
-		// pane.getChildren()browser.(browserView, BorderLayout.CENTER);
-		Scene scene = new Scene(pane, 500, 400);
-		primaryStage.setTitle("ChartIQ Demo");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent event) {
-				Platform.exit();
-			}
-		});
-
-		button.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				String symbol = "IBM";
-				chartIQ.setSymbol("AAPL");
-				// browser.executeJavaScript("test2();");
-				// browser.executeJavaScript("alert('asdf')");
-				// browser.executeJavaScript("stxx.newChart('IBM');");
-				// browser.executeJavaScript("window.callNewChart('ibm');");
-			}
-		});
-
-		chartIQ.setDataSource(new ChartIQ.DataSource() {
-			@Override
-			public void pullUpdateData(Map<String, Object> params, DataSourceCallback callback) {
-				loadChartData(params, callback);
-			}
-
-			@Override
-			public void pullPaginationData(Map<String, Object> params, DataSourceCallback callback) {
-				loadChartData(params, callback);
-			}
-
-			@Override
-			public void pullInitialData(Map<String, Object> params, DataSourceCallback callback) {
-				loadChartData(params, callback);
-			}
-		});
+				
+	 
+//		Label label1 = new Label("Symbol:");
+//
+//		primaryStage.setTitle("ChartIQ Example");
+//		TextField textField = new TextField();
+//		textField.setPrefSize(150, 28);
+//		textField.setPrefColumnCount(15);
+//		textField.setPromptText("Enter Symbol");
+//		textField.setFocusTraversable(false);
+//		Button button = new Button("Lookup");
+//		HBox hb = new HBox();
+//		ComboBox periodicity = new ComboBox();
+//		periodicity.getItems().addAll("1 Day", "1 Week", "1 Month", "1 Minute");
+//		periodicity.getSelectionModel().select(0);
+//		hb.getChildren().addAll(textField, button, periodicity);
+//		hb.setSpacing(10);
+//		hb.setStyle("-fx-background-color: #336699;");
+//
+//		// Browser browser = new Browser();
+//		// BrowserView view = new BrowserView(browser);
+//		//
+//		// BorderPane borderPane = new BorderPane();
+//		// borderPane.setCenter(view);
+//		// borderPane.setTop(hb);
+//		//
+//		// String remoteDebuggingURL = browser.getRemoteDebuggingURL();
+//		//
+//		// JFrame frame1 = new JFrame();
+//		// frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		// frame1.add(view, BorderLayout.CENTER);
+//		// frame1.setSize(700, 500);
+//		// frame1.setLocationRelativeTo(null);
+//		// frame1.setVisible(true);
+//
+//		// Scene scene = new Scene(borderPane, 700, 500);
+//		// primaryStage.setScene(scene);
+//		// primaryStage.show();
+//
+//		chartIQ.setBrowser("http://10.0.2.15:8080/default/sample-template-native-sdk.html");
+//		BrowserView browserView = chartIQ.getBrowserView();
+//
+//		// Creates another Browser instance and loads the remote Developer
+//		// Tools URL to access HTML inspector.
+//		// Browser browser2 = new Browser();
+//		// BrowserView browserView2 = new BrowserView(browser2);
+//
+//		// JFrame frame2 = new JFrame();
+//		// frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		// frame2.add(browserView2, BorderLayout.CENTER);
+//		// frame2.setSize(700, 500);
+//		// frame2.setLocationRelativeTo(null);
+//		// frame2.setVisible(true);
+//
+//		// browser2.loadURL(browser.getRemoteDebuggingURL());
+//
+//		System.out.println(chartIQ.getBrowser().getRemoteDebuggingURL());
+//
+//		BorderPane pane = new BorderPane();
+//		pane.setTop(hb);
+//		pane.setCenter(browserView);
+//		// pane.getChildren()browser.(browserView, BorderLayout.CENTER);
+//		Scene scene = new Scene(pane, 800, 500);
+//		primaryStage.setTitle("ChartIQ Demo");
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
+//
+//		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//			@Override
+//			public void handle(WindowEvent event) {
+//				Platform.exit();
+//			}
+//		});
+//
+//		button.setOnAction(new EventHandler<ActionEvent>() {
+//			@Override
+//			public void handle(ActionEvent e) {
+//				String symbol = "IBM";
+//				chartIQ.setSymbol("AAPL");
+//				// browser.executeJavaScript("test2();");
+//				// browser.executeJavaScript("alert('asdf')");
+//				// browser.executeJavaScript("stxx.newChart('IBM');");
+//				// browser.executeJavaScript("window.callNewChart('ibm');");
+//			}
+//		});
+//
+//		chartIQ.setDataSource(new ChartIQ.DataSource() {
+//			@Override
+//			public void pullUpdateData(Map<String, Object> params, DataSourceCallback callback) {
+//				loadChartData(params, callback);
+//			}
+//
+//			@Override
+//			public void pullPaginationData(Map<String, Object> params, DataSourceCallback callback) {
+//				loadChartData(params, callback);
+//			}
+//
+//			@Override
+//			public void pullInitialData(Map<String, Object> params, DataSourceCallback callback) {
+//				loadChartData(params, callback);
+//			}
+//		});
 	}
 
 	private void loadChartData(Map<String, Object> params, final ChartIQ.DataSourceCallback callback) {
---
+++ src/com/chartiq/sample/SampleController.java
@@ -0,0 +1,12 @@
+package com.chartiq.sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class SampleController {
+	
+	@FXML
+	private TextArea outputText;
+
+}
+ 
